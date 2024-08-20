ca47ae8329da5e8fd5f9e62719a04354d3036db5
Erichy-dev <erichnyaga1@gmail.com>
2024-08-20 09:17:39 +0300

    Initial Commit

---
+++ Rentals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rentals
+//
+//  Created by mac on 8/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rentals/RentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentalsApp.swift
+//  Rentals
+//
+//  Created by mac on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct RentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
